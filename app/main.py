fae2b17ffd5986794a018600532cdca10459daaf
Nestor Martinez <nmartinezc1800@alumno.ipn.mx>
2024-08-06 17:20:35 -0600

    Responsed added

--- app/main.py
+++ app/main.py
@@ -17,29 +17,29 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, tags=["posts"])
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse, tags=["posts"])
 async def create_posts(post: schemas.PostBase, db: Session = Depends(get_db)):
     new_post = models.Posts(**post.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
-@app.get("/posts", tags=["posts"])
+@app.get("/posts", response_model= list[schemas.PostResponse], tags=["posts"])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Posts).all()
-    return {"data": posts}
+    return posts
 
 
-@app.get("/posts/{post_id}", tags=["posts"])
+@app.get("/posts/{post_id}", response_model=schemas.PostResponse, tags=["posts"])
 async def get_post(post_id: int, db: Session = Depends(get_db)):
     r = db.query(models.Posts).filter(models.Posts.id == literal(post_id)).first()
     if not r:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Item not found",
                             headers={"X-Error": "There goes my error"})
-    return {"data": r}
+    return r
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT, tags=["posts"])
@@ -54,7 +54,7 @@ async def delete_post(post_id: int, db: Session = Depends(get_db)):
     return
 
 
-@app.put("/posts/{post_id}", tags=["posts"])
+@app.put("/posts/{post_id}", response_model=schemas.PostResponse, tags=["posts"])
 async def update_post(post_id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post = db.query(models.Posts).filter(models.Posts.id == literal(post_id))
     if not post.first():
@@ -63,4 +63,4 @@ async def update_post(post_id: int, updated_post: schemas.PostCreate, db: Sessio
                             headers={"X-Error": "There goes my error"})
     post.update(updated_post.model_dump(), synchronize_session=False)
     db.commit()
-    return {"data": db.query(models.Posts).filter(models.Posts.id == literal(post_id)).first()}
+    return post.first()
