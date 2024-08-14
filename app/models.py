17a337347dc0aa1d0e3e65d3a6b748d986c10294
Nestor Martinez <nmartinezc1800@alumno.ipn.mx>
2024-08-14 14:54:05 -0600

    Implementing only create posts

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app.database import Base
-from sqlalchemy import String, text
+from sqlalchemy import String, text, ForeignKey
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
 from datetime import datetime
 
 
@@ -13,12 +13,7 @@ class Posts(Base):
     content: Mapped[str] = mapped_column(String(255))
     published: Mapped[bool] = mapped_column(server_default='1')
     created_at: Mapped[datetime] = mapped_column(server_default=text('CURRENT_TIMESTAMP'))
-
-    """id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(100), nullable=False)
-    content = Column(String(255), nullable=False)
-    published = Column(Boolean, server_default='1')
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))"""
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 
 
 class Users(Base):
@@ -28,10 +23,4 @@ class Users(Base):
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(255))
     created_at: Mapped[datetime] = mapped_column(server_default=text('CURRENT_TIMESTAMP'))
-
-    """
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(100), nullable=False, unique=True)
-    password = Column(String(255), nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('CURRENT_TIMESTAMP'))
-    """
+    posts: Mapped[list["Posts"]] = relationship()
